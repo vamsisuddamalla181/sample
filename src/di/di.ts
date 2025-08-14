7bad8eb116982335829d326761d7ed503b7fc541
bhargavikakarla00183 <bhargavi.kakarla@tekreant.com>
2025-08-14 18:00:17 +0530

    added di

--- src/di/di.ts
+++ src/di/di.ts
@@ -1,5 +1,10 @@
 import { container } from "tsyringe";
 import { UserController } from "../controller/usercontrollers.ts";
 import { userrepository } from "../repository/userrepository.ts";
+import { TaskController } from "../controller/taskcontroler.ts";
+import { taskRepository } from "../repository/taskrepository.ts";
 container.register(userrepository,{useClass:userrepository})
 container.register(UserController,{useClass:UserController})
+container.register(taskRepository,{useClass:taskRepository})
+container.register(TaskController,{useClass:TaskController})
+
--- src/routes/table_routes.ts
+++ src/routes/table_routes.ts
@@ -1,15 +1,16 @@
 import express from "express"
 const route=express.Router()
-import taskcontroler from "../controller/taskcontroler.ts"
+import { TaskController } from "../controller/taskcontroler.ts"
 import { UserController } from "../controller/usercontrollers.ts"
 import { container } from "tsyringe"
 const controller=container.resolve(UserController)
+const taskcontroller=container.resolve(TaskController)
 route.post("/post",controller.createUser)
 route.get("/getall",controller.getAllUsers)
 route.get("/getbyid/:userID",controller.getById)
 route.post("/update/:userId",controller.getByIdAndUpdate)
-route.post("/posttask",taskcontroler.createtask)
-route.post("/assign/:userId",taskcontroler.assignTask)
-route.get("/user/:userId",taskcontroler.getTasksForUser)
-route.post("/taskuser/:userId",taskcontroler.assignTaskforUser)
+route.post("/posttask",taskcontroller.createTask)
+route.post("/assign/:userId",taskcontroller.assignTask)
+route.get("/user/:userId",taskcontroller.getTasksForUser)
+route.post("/taskuser/:userId",taskcontroller.assignUnassignedTasks)
 export default route
