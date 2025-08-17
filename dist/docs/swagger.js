8dc10e3d3076c01c652ac24099d02a2556a85a7e
vamsisuddamalla181 <vamsikrishnanaidu.suddamalla@tekreant.com>
2025-08-17 14:44:33 +0530

    swagger added

--- dist/docs/swagger.js
+++ dist/docs/swagger.js
@@ -1,21 +1,28 @@
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerUi from "swagger-ui-express";
-// const swaggerDefinition = {
-//   openapi: "3.0.0",
-//   info: {
-//     title: "Sample API",
-//     version: "1.0.0",
-//     description: "API documentation for Sample project",
-//   },
-//   servers: [
-//     {
-//       url: "http://localhost:8000",
-//     },
-//   ],
-// };
-// const options = {
-//   swaggerDefinition,
-//   apis: ["./src/modules/**/*.ts"], 
-// };
-// const swaggerSpec = swaggerJSDoc(options);
-// export { swaggerUi, swaggerSpec };
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.swaggerSpec = exports.swaggerUi = void 0;
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+exports.swaggerUi = swagger_ui_express_1.default;
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "Sample API",
+        version: "1.0.0",
+        description: "API documentation for Sample project",
+    },
+    servers: [
+        {
+            url: "http://localhost:8000",
+        },
+    ],
+};
+const options = {
+    swaggerDefinition,
+    apis: ["./src/modules/**/*.ts"],
+};
+const swaggerSpec = (0, swagger_jsdoc_1.default)(options);
+exports.swaggerSpec = swaggerSpec;
--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,25 +1,25 @@
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
-// const swaggerDefinition = {
-//   openapi: "3.0.0",
-//   info: {
-//     title: "Sample API",
-//     version: "1.0.0",
-//     description: "API documentation for Sample project",
-//   },
-//   servers: [
-//     {
-//       url: "http://localhost:8000",
-//     },
-//   ],
-// };
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Sample API",
+    version: "1.0.0",
+    description: "API documentation for Sample project",
+  },
+  servers: [
+    {
+      url: "http://localhost:8000",
+    },
+  ],
+};
 
-// const options = {
-//   swaggerDefinition,
-//   apis: ["./src/modules/**/*.ts"], 
-// };
+const options = {
+  swaggerDefinition,
+  apis: ["./src/modules/**/*.ts"], 
+};
 
-// const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(options);
 
-// export { swaggerUi, swaggerSpec };
+export { swaggerUi, swaggerSpec };
